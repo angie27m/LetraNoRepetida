39d8e5fe1551c32cd567d0ea55f12c99dee7a528
Manrique <angie27manrique@gmail.com>
2019-08-25 00:00:40 -0500

    Impresión de la primer letra que no se repite en el texto

---
+++ src/main/java/com/udec/primernorepetido/Procedimiento.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.primernorepetido;
+
+import java.util.Scanner;
+
+/**
+ * Esta clase se encarga de encontrar la primer letra que no se repite dentro de
+ * una cadena de texto haciendo una lectura letra por letra hasta el final de la
+ * misma
+ *
+ * @author Angie Manrique
+ * @version 1.0
+ */
+public class Procedimiento {
+
+    /**
+     * La variable cadenaIngresada guarda el texto ingresado por el usuario
+     */
+    private String cadenaIngresada;
+    /**
+     * La variable tipo char array es usada para conocer las letras que se
+     * repiten
+     */
+    private char[] cadenaSeparada;
+    /**
+     * Contiene la primer letra que no se repite
+     */
+    private char letraSinRepetir;
+
+    /**
+     * Método constructor: Ingreso de la cadena de texto
+     */
+    public Procedimiento() {
+        String seguir = "";
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Ingrese el texto:  ");
+            cadenaIngresada = scanner.nextLine();
+            separacion();
+            Scanner respuesta = new Scanner(System.in);
+            System.out.println("¿Desea continuar? S=>Si o N=>No");
+            seguir = respuesta.nextLine();
+        } while (seguir.equals("s") || seguir.equals("S"));
+    }
+
+    /**
+     * Este método se encarga de separar por caracteres el string ingresado por
+     * el usuario
+     *
+     * @param primeraVez esta variable es usada para identificar si es la primer
+     * letra del texto
+     */
+    public void separacion() {
+        cadenaSeparada = cadenaIngresada.toCharArray();
+        boolean primeraVez = true;
+        letraSinRepetir = cadenaSeparada[0];
+        for (int i = 0; i < cadenaIngresada.length(); i++) {
+            if (primeraVez) {
+                letraSinRepetir = cadenaSeparada[i];
+                primeraVez = false;
+            } else {
+                if (letraSinRepetir == cadenaSeparada[i]) {
+                    letraSinRepetir = validarUnica(i);
+                }
+                if (letraSinRepetir == 0) {
+                    letraSinRepetir = validarRepetidas(cadenaSeparada[i]);
+                }
+            }
+            impresion(cadenaSeparada[i]);
+        }
+    }
+
+    /**
+     * Este método hace la validación de las letras dentro de la cadena
+     * verificando si son iguales o no
+     *
+     * @param letraSinRepetir esta variable contiene la primer letra que no ha
+     * sido repetida
+     * @param letraUnica contiene la posicion del array en donde está la letra
+     * que no ha sido repetida
+     * @param acumulador variable usada para saber la cantidad de veces que se
+     * ha repetido una letra dentro del texto
+     * @return retorna la primer letra que encuentra sin repetirse
+     */
+    private char validarUnica(int letraUnica) {
+        int acumulador;
+        for (int i = 0; i < letraUnica; i++) {
+            if (letraSinRepetir != cadenaSeparada[i]) {
+                acumulador = 0;
+                for (int j = 0; j < letraUnica; j++) {
+                    if (cadenaSeparada[i] == cadenaSeparada[j]) {
+                        acumulador++;
+                    }
+                }
+                if (acumulador == 1) {
+                    return cadenaSeparada[i];
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Busca las letras repetidas haciendo uso de un contador, cuando el
+     * contador es igual a 1 retorna la letra guardada
+     *
+     * @param caracter
+     * @return devuelve el caracter que se repite
+     */
+    private char validarRepetidas(char caracter) {
+        int contador = 0;
+        for (char dato : cadenaSeparada) {
+            if (dato == caracter) {
+                contador++;
+            }
+        }
+        if (contador == 1) {
+            return caracter;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Este método se encarga de las impresiones del programa
+     *
+     * @param letraSinRepetir esta variable contiene la primer letra que no ha
+     * sido repetida
+     * @param caracter esta variable guarda la letra que esta siendo leída
+     */
+    private void impresion(char caracter) {
+        if (letraSinRepetir == 0) {
+            System.out.println("Reading: " + caracter + " Todos las letras se repiten");
+        } else {
+            System.out.println("Reading: " + caracter + " La primer letra que no se repite es: " + letraSinRepetir);
+        }
+    }
+}
